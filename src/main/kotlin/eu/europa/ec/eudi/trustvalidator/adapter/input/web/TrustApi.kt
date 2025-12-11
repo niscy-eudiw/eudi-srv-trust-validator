3380e8d414607bd733ee18e1b35d6da54e7aa005
Anastasios Tsantoulis <anastasios.tsantoulis@netcompany.com>
2025-12-11 16:16:35 +0200

    Add context load test and fix package information (#5)

---
+++ src/main/kotlin/eu/europa/ec/eudi/trustvalidator/adapter/input/web/TrustApi.kt
@@ -0,0 +1,84 @@
+/*
+ * Copyright (c) 2023 European Commission
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package eu.europa.ec.eudi.trustvalidator.adapter.input.web
+
+import arrow.core.Either
+import arrow.core.NonEmptyList
+import eu.europa.ec.eudi.trustvalidator.adapter.out.x5c.X509CertificateChainSerializer
+import eu.europa.ec.eudi.trustvalidator.domain.ProviderType
+import eu.europa.ec.eudi.trustvalidator.port.input.VerifyTrust
+import kotlinx.serialization.Serializable
+import org.springframework.http.MediaType.APPLICATION_JSON
+import org.springframework.web.reactive.function.server.*
+import org.springframework.web.reactive.function.server.RequestPredicates.version
+import org.springframework.web.reactive.function.server.ServerResponse.badRequest
+import org.springframework.web.reactive.function.server.ServerResponse.ok
+import java.security.cert.X509Certificate
+
+internal class TrustApi(
+    private val verifyTrust: VerifyTrust,
+) {
+    val route: RouterFunction<ServerResponse> = coRouter {
+        POST(
+            TRUST_QUERY,
+            contentType(APPLICATION_JSON) and accept(APPLICATION_JSON) and version("1.0.0"),
+            ::trustQuery,
+        )
+    }
+
+    private suspend fun trustQuery(request: ServerRequest): ServerResponse {
+        val body = Either
+            .catch { request.awaitBody<TrustQueryRequest>() }
+            .fold(
+                ifLeft = { e ->
+                    return badRequest().json().bodyValueAndAwait(ErrorResponse("Invalid request body: ${e.message}"))
+                },
+                ifRight = { it },
+            )
+
+        val serviceType = body.serviceType.toDomain()
+
+        return verifyTrust(serviceType, body.x5c).fold(
+            ifLeft = { error ->
+                badRequest().json().bodyValueAndAwait(error)
+            },
+            ifRight = { trusted ->
+                ok().contentType(APPLICATION_JSON).bodyValueAndAwait(TrustQueryResponse(trusted))
+            },
+        )
+    }
+
+    companion object {
+        const val TRUST_QUERY = "/trust"
+    }
+}
+
+@Serializable
+internal data class TrustQueryRequest(
+    @Serializable(with = X509CertificateChainSerializer::class)
+    val x5c: NonEmptyList<X509Certificate>,
+    val serviceType: ProviderType,
+)
+
+@Serializable
+internal data class TrustQueryResponse(
+    val trusted: Boolean,
+)
+
+@Serializable
+internal data class ErrorResponse(
+    val error: String,
+)
---
+++ src/main/kotlin/eu/europa/ec/eudi/trustvalidator/adapter/out/cert/TrustSources.kt
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2023 European Commission
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package eu.europa.ec.eudi.trustvalidator.adapter.out.cert
+
+import eu.europa.ec.eudi.trustvalidator.domain.ServiceType
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
+import java.security.cert.X509Certificate
+
+/**
+ * Functional interface for providing the appropriate X5CShouldBe trust source based on trust provider type.
+ */
+fun interface ProvideTrustSource {
+    suspend operator fun invoke(type: String): List<X509Certificate>?
+}
+
+class TrustSources(
+    private val certsPerType: MutableMap<ServiceType, List<X509Certificate>> = mutableMapOf(),
+) : ProvideTrustSource {
+
+    private val mutex = Mutex()
+
+    suspend fun updateWithX5CShouldBe(pattern: ServiceType, certs: List<X509Certificate>) {
+        mutex.withLock {
+            certsPerType[pattern] = certs
+        }
+    }
+
+    /**
+     * Implementation of TrustSourceProvider
+     * Retrieves the X5CShouldBe for the given document type.
+     */
+    override suspend fun invoke(type: String): List<X509Certificate>? =
+        mutex.withLock {
+            certsPerType.entries
+                .firstOrNull { (pattern, _) -> pattern.value == type }
+                ?.value
+        }
+}
